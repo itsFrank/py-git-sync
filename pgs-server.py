f60bcd232c04c3c136951d33d71cb091dcb9c39b
itsfrank <francis.obrien@mail.utoronto.ca>
2019-03-30 17:02:49 -0400

    active dev

--- pgs-server.py
+++ pgs-server.py
@@ -6,5 +6,5 @@ from subprocess import PIPE
 p_pull = subprocess.run(["git", "pull", "sync", "frank-dev"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
 found = p_pull.stdout.find("Already up-to-date.")
 
-if (found != -1):
+if (found == -1):
     subprocess.run(["make"])
