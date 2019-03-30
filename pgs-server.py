4bf015ba576ff44a8609d6252b44316ca4b76603
itsfrank <francis.obrien@mail.utoronto.ca>
2019-03-30 17:06:12 -0400

    active dev

--- pgs-server.py
+++ pgs-server.py
@@ -3,12 +3,17 @@ import os
 from datetime import datetime
 from subprocess import PIPE
 import sys
+import time
 
-for val in sys.argv:
-    print(val)
+timeout_s = 3
+if (len(sys.argv)>1):
+    timeout_s = int(sys.argv[1])
 
-p_pull = subprocess.run(["git", "pull", "sync", "frank-dev"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-found = p_pull.stdout.find("Already up-to-date.")
+while True:
+    p_pull = subprocess.run(["git", "pull", "sync", "frank-dev"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    found = p_pull.stdout.find("Already up-to-date.")
 
-if (found == -1):
-    subprocess.run(["make"])
+    if (found == -1):
+        subprocess.run(["make"])
+    
+    time.sleep(timeout_s)
