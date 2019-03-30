4c894feec0fb6db2c1fe84d35905563af74f8eef
itsfrank <francis.obrien@mail.utoronto.ca>
2019-03-30 17:03:42 -0400

    active dev

--- pgs-server.py
+++ pgs-server.py
@@ -2,6 +2,10 @@ import subprocess
 import os
 from datetime import datetime
 from subprocess import PIPE
+import sys
+
+for val in sys.argv:
+    print(val)
 
 p_pull = subprocess.run(["git", "pull", "sync", "frank-dev"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
 found = p_pull.stdout.find("Already up-to-date.")
